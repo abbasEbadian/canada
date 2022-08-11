d7a4947587f84f5abdb933246c26a0d5d74c7b1c
abbasEbadian <abbasebadiann@gmail.com>
2022-08-11 15:56:38 +0430

    admin

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,9 @@ import Home from './Home'
 import Centralized from './Centralized'
 import CentralizedForex from './CentralizedForex'
 import Blogs from './Blogs'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+import Dashboard from './Admin/Dashboard'
 
 function Router() {
   return (
@@ -13,6 +16,9 @@ function Router() {
             <Route path="/center" element={<Centralized/>} />
             <Route path="/center-forex" element={<CentralizedForex/>} />
             <Route path="/blog" element={<Blogs/>} />
+            <Route path="/signin" element={<SignIn/>} />
+            <Route path="/signup" element={<SignUp/>} />
+            <Route path="/admin" element={<Dashboard/>} />
         </Routes>
     </HashRouter>
   )
