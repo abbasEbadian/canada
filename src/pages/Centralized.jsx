1fa91d0fe366c73d8d46737d73eb0880255ef572
abbasEbadian <abbasebadiann@gmail.com>
2022-08-27 01:08:42 +0430

    center page features and press

--- src/pages/Centralized.jsx
+++ src/pages/Centralized.jsx
@@ -10,10 +10,23 @@ import AssetsCentralized from '../components/SubCentralized/AssetsCentralized'
 import FocusTradeCentralized from '../components/SubCentralized/FocusTradeCentralized'
 import axios from 'axios'
 import { MasterContext } from '.'
+import NewsSlider from '../components/SubBlogs/NewsSlider'
+import styled from '@emotion/styled'
 
-
+const CentralizedImage = styled.div`
+margin-bottom: 64px;
+  img{
+    max-width: 400px;
+  }
+  @media (min-width: 992px){
+    img{
+      max-width: unset;
+    }
+  }
+`
 function Centralized() {
   const [managers, setManagers] = useState([])
+  const [blogs, setBlogs] = useState([])
   const { setLoading } = useContext(MasterContext)
 
   useEffect(() => {
@@ -23,7 +36,12 @@ function Centralized() {
         setManagers(data)
       })
       .catch(f => console.log(f))
-   
+
+    axios.get('/api/v1/blogs/press/')
+      .then(({ data }) => {
+        setBlogs(data)
+      })
+      .catch(f => console.log(f))
       .finally(f => setLoading(false))
   }, [])
   return (
@@ -42,7 +60,7 @@ function Centralized() {
       {/* assets table */}
       <div className="table-assets-body">
         <div className="container">
-          <HeaderTable activePage={'forex'}/>
+          <HeaderTable activePage={'forex'} />
           <div className="chart-table">
             <div className="row">
               <div className="col-12 col-lg-4">
@@ -55,12 +73,21 @@ function Centralized() {
           </div>
         </div>
       </div>
+      <CentralizedImage className="container text-center ">
+        <picture  width={"100%"}>
+          <source media="(max-width:992px)" srcset="img/mobile/Features.png"  />
+          <img src="img/Features.png" className="text-center" alt="features" width={"100%"}/>
+        </picture>
+  
+      </CentralizedImage>
 
       {/* Find manager */}
       <SliderManager managers={managers} _class="manage-slider" />
 
       {/* technology */}
       <TechnologyCentralized />
+
+      <NewsSlider postList={blogs} class_prepend={'pressList'} title={"In Press"} />
 
       <Footer />
 
