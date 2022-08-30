ac9224b7bef75b19442a1a8680f3d178005052af
awrsh <arashzarandi@gmail.com>
2022-08-30 14:16:15 +0430

    change logo - text

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import axios from 'axios'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
-if (process.env.NODE_ENV === 'development' ){
+if (false && process.env.NODE_ENV === 'development' ){
   axios.defaults.baseURL = 'http://localhost:8000'
 }else{
   axios.defaults.baseURL = 'http://82.115.19.221/'
